423cf55a9a5b2abc11548fed2d76e94c62b2a763
Darz <54253370+djgraphics28@users.noreply.github.com>
2025-05-18 16:31:28 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\BarangayController;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    return redirect()->route('login');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
